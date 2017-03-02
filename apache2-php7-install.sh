f072674a83dbe1947e0f9681c340a184292a05a9
[sagespidy] <[shailender.birmaan2@gmail.com]>
2017-03-02 15:33:05 +0530

    working fine except directory permission for /var/www/html

--- apache2-php7-install.sh
+++ apache2-php7-install.sh
@@ -24,6 +24,7 @@ echo " 			Installing Apache... "
 
 apt-get install -y apache2
 apt-get install libapache2-mod-php7.0 -y
+apt-get install libapache2-modsecurity -y
 
 
 echo -e "\n\n\n"
@@ -58,10 +59,30 @@ a2enmod php7.0
 a2enconf php7.0-fpm
 a2enmod rewrite
 a2enmod proxy_fcgi setenv
+a2enmod headers
+a2enmod mod-security
+
 
 echo -e "\n\n\n"
 echo "		Required Modules Enabled"
 echo -e "\n\n\n"
+# Making Apache Server Secure
+
+# Stop displaying Apache Version
+echo "ServerSignature Off" >> /etc/apache2/apache2.conf
+
+# Show servertoken as Apache
+echo "ServerTokens Prod" >> /etc/apache2/apache2.conf
+echo " Header set X-XSS-Protection "1; mode=block" " >> /etc/apache2/apache2.conf
+echo "Header always set X-Content-Type-Options "nosniff" " >>/etc/apache2/apache2.conf
+echo "Header always set Strict-Transport-Security "max-age=63072000;includeSubDomains"" >>/etc/apache2/apache2.conf
+
+# Stop Click JAcking
+echo "Header always append X-Frame-Options SAMEORIGIN" >> /etc/apache2/apache2.conf
+
+#Enable Directory permissions
+echo -e "<Directory /var/www/html> \n Options  FollowSymLinks\n AllowOverride all \n Require all granted \n </Directory>"  >> /etc/apache2/sites-enabled/000-default.conf
+
 echo " 			Restarting Apache "
 echo -e "\n\n\n"
 
@@ -69,6 +90,8 @@ systemctl restart apache2
 
 echo "			Apache Restarted"
 echo -e " \n\n\n "
+echo -e "\n\n\n\n\n\n"
+echo -e "\n\n\n\n\n\n"
 
 echo "Creating a New User...."
 
@@ -95,6 +118,10 @@ echo "$usr_name:$usr_name-123#@"|chpasswd
 
 sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
 
+service ssh restart
+
+echo -e "\n\n\n\n\n\n"
+echo -e "\n\n\n\n\n\n"
 echo "SSH via Password Authentication enabled"
 
 echo "$usr_name  ALL=NOPASSWD:ALL #Sudo rights for $usr_name  " >>/etc/sudoers
@@ -106,7 +133,16 @@ echo -e "\n\n\n\n\n\n"
 echo "Synchronizing System Time"
 
 apt-get install -y ntp
-
-echo "Enter Hostname. This will be used to identify different servers i.e Dev,Qa,Satging, Production--- "
+echo -e "\n\n\n\n\n\n"
+echo -e "\n\n\n\n\n\n"
+echo "Enter Hostname. To identify different servers i.e Dev,Qa,Satging, Production ..... "
+# Get Hostname from user
 read hname
+
 hostnamectl set-hostname $hname
+
+# Making enteries in /etc/hosts file
+echo "127.0.0.1 $hname" >> /etc/hosts
+
+# tell apache to use  localhost as Hostname
+echo "ServerName localhost" >> /etc/apache2/apache2.conf
